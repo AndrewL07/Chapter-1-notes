e0149f273557017188e6e161cda8e34f453a7be4
xlsj <xlsj@fresnou.org>
2023-02-17 10:27:45 -0800

    added while loops notes 1

---
+++ whileloops1.js
@@ -0,0 +1,72 @@
+const readline= require('readline-sync');
+
+// //1
+// let x = 0;
+// while (x<26) {
+//     console.log(x);
+//     x++;
+// }
+//
+// //2
+// let y = 0;
+// while (y<51) {
+//     console.log(y);
+//     y++;
+// }
+//
+// //3
+// let w = -10;
+// while (w<11) {
+//     console.log(w);
+//     w++;
+// }
+//
+//4
+// let s = 0;
+// while (s>-51) {
+//     console.log(s);
+//     s--;
+// }
+
+//5
+// let p = 0;
+// while (p<26) {
+//     console.log(p);
+//     p+=2;
+// }
+
+//6
+// let r = 101;
+// while (r>-1) {
+//     console.log(r);
+//     r -= 2;
+// }
+
+//7
+// let a = 0;
+// while (a<99) {
+//     console.log(a);
+//     a += 3;
+// }
+
+//8
+let b = 0;
+while (b<999) {
+    console.log(b;
+    b += 5;
+}
+
+
+//9
+let start = 0;
+while (start <=100){
+    if(start <10){
+        console.log(start)
+    } else if (start > 25){
+        console.log(start)
+    }
+
+    start++;
+}
+
+//10
